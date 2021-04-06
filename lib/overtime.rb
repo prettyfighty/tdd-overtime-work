42f704e531b21e5ae757105df5af3e9da75b9869
Lailai <prettyfighty@gmail.com>
2021-04-06 23:11:48 +0800

    在overtime.rb新增attr_reader整理payment內容

--- lib/overtime.rb
+++ lib/overtime.rb
@@ -1,4 +1,5 @@
 class Overtime
+  attr_reader :labor, :day_type
 
   def initialize(day_type)
     @day_type = day_type
@@ -14,41 +15,41 @@ class Overtime
   end
 
   def payment
-    worked_for_hours = ((@labor.worked_for_seconds) / 3600.0).round
-    case @day_type
+    worked_for_hours = ((labor.worked_for_seconds) / 3600.0).round
+    case day_type
     when "平常日"
       case worked_for_hours
       when 0..2
-        (@labor.salary_per_hour * worked_for_hours * 1.34).round
+        (labor.salary_per_hour * worked_for_hours * 1.34).round
       when 3..4
-        (@labor.salary_per_hour * 2 * 1.34).round + (@labor.salary_per_hour * (worked_for_hours - 2) * 1.67).round
+        (labor.salary_per_hour * 2 * 1.34).round + (labor.salary_per_hour * (worked_for_hours - 2) * 1.67).round
       end      
     when "休息日"
       case worked_for_hours
       when 0..2
-        (@labor.salary_per_hour * worked_for_hours * 1.34).round
+        (labor.salary_per_hour * worked_for_hours * 1.34).round
       when 3..8
-        (@labor.salary_per_hour * 2 * 1.34).round + (@labor.salary_per_hour * (worked_for_hours - 2) * 1.67).round
+        (labor.salary_per_hour * 2 * 1.34).round + (labor.salary_per_hour * (worked_for_hours - 2) * 1.67).round
       when 9..12
-        (@labor.salary_per_hour * 2 * 1.34).round + (@labor.salary_per_hour * 6 * 1.67).round + (@labor.salary_per_hour * (worked_for_hours - 8) * 2.67).round
+        (labor.salary_per_hour * 2 * 1.34).round + (labor.salary_per_hour * 6 * 1.67).round + (labor.salary_per_hour * (worked_for_hours - 8) * 2.67).round
       end
     when "休假日"
       case worked_for_hours
       when 0..8
-        @labor.salary_per_hour * 8
+        labor.salary_per_hour * 8
       when 9..10
-        (@labor.salary_per_hour * 8) + (@labor.salary_per_hour * (worked_for_hours - 8) * 2.34).round
+        (labor.salary_per_hour * 8) + (labor.salary_per_hour * (worked_for_hours - 8) * 2.34).round
       when 11..12
-        (@labor.salary_per_hour * 8) + (@labor.salary_per_hour * 2 * 2.34).round + (@labor.salary_per_hour * (worked_for_hours - 10) * 2.67).round
+        (labor.salary_per_hour * 8) + (labor.salary_per_hour * 2 * 2.34).round + (labor.salary_per_hour * (worked_for_hours - 10) * 2.67).round
       end
     when "假日時因天災、事變或突發事件"
       case worked_for_hours
       when 0..8
-        @labor.salary_per_hour * worked_for_hours * 2
+        labor.salary_per_hour * worked_for_hours * 2
       when 9..10
-        (@labor.salary_per_hour * 8 * 2) + (@labor.salary_per_hour * (worked_for_hours - 8) * 2.34).round
+        (labor.salary_per_hour * 8 * 2) + (labor.salary_per_hour * (worked_for_hours - 8) * 2.34).round
       when 11..12
-        (@labor.salary_per_hour * 8 * 2) + (@labor.salary_per_hour * 2 * 2.34).round + (@labor.salary_per_hour * (worked_for_hours - 10) * 2.67).round
+        (labor.salary_per_hour * 8 * 2) + (labor.salary_per_hour * 2 * 2.34).round + (labor.salary_per_hour * (worked_for_hours - 10) * 2.67).round
       end
     end
   end
